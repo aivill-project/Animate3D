70d82aa493366d2501068b1d6dc0544e138838dc
a22106 <bk22106@gmail.com>
2022-07-27 14:10:29 +0900

    update ignore bash

--- IMavatar1/download_data.bash
+++ IMavatar1/download_data.bash
@@ -5,8 +5,9 @@ cd ./data/datasets/
 wget https://dataset.ait.ethz.ch/downloads/IMavatar_data/data/yufeng.zip
 #curl -k https://dataset.ait.ethz.ch/downloads/IMavatar_data/data/yufeng.zip -o yufeng.zip
 unzip yufeng.zip
-mv yufeng.zip ../experiments/yufeng.zip
 cd ../experiments/
+wget https://dataset.ait.ethz.ch/downloads/IMavatar_data/checkpoint/yufeng.zip
+#curl -k https://dataset.ait.ethz.ch/downloads/IMavatar_data/checkpoint/yufeng.zip -o yufeng.zip
 unzip yufeng.zip
 cd ..
 python addgitignorepath.py --path IMavatar1/data/
